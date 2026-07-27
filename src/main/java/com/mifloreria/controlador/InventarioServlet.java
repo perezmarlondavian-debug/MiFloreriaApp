b89101436032b3479e62137447969bbf4ca0d5cb
Marlon Perez <perezmarlondavian@gmail.com.com>
2026-07-27 01:25:34 -0500

    Se agrega documentacion de APIs y archivos para la evidencia EV03

--- src/main/java/com/mifloreria/controlador/InventarioServlet.java
+++ src/main/java/com/mifloreria/controlador/InventarioServlet.java
@@ -1,33 +1,53 @@
 package com.mifloreria.controlador;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
+import com.google.gson.Gson;
+import com.mifloreria.config.Conexion;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-
-@WebServlet(name = "InventarioServlet", urlPatterns = {"/inventario"})
+@WebServlet("/api/inventario/*")
 public class InventarioServlet extends HttpServlet {
 
-   
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-        
-        System.out.println("El gerente (Servlet) recibió la orden de mostrar el inventario.");
-        
-      
-        request.getRequestDispatcher("inventario.jsp").forward(request, response);
-    }
+    private static final Gson gson = new Gson();
 
-   
-    
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        List<String> listaProductos = new ArrayList<>();
+        String sql = "SELECT * FROM flores"; 
+
+       
+        Conexion conexionDB = new Conexion();
+
+        try (Connection conn = conexionDB.getConexion();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                String nombre = rs.getString("nombre");
+                double precio = rs.getDouble("precio");
+                listaProductos.add("Producto: " + nombre + " - Precio: " + precio);
+            }
+
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().write(gson.toJson(listaProductos));
+
+        } catch (SQLException e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write(gson.toJson(Map.of("error", "Error al consultar la base de datos: " + e.getMessage())));
+        }
     }
 }
